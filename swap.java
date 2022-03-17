033616a31298e92fbcc4d5b63c884db0a7a99aec
sanketshirode95 <101618038+sanketshirode95@users.noreply.github.com>
2022-03-17 11:46:10 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,20 @@
+package day3;
+
+public class swap
+{
+
+	public static void main(String[] args)
+	{
+      int 	a=10;
+      int   b=20 ;
+      System.out.println("Before swap"  +" "+   a  + "  " +    b);  
+		
+      a= a+b;     
+	  b= a-b;  
+	  a= a-b ;  
+	  System.out.println("After swap"  +" "+   a   +  "   " +   b);   
+	
+
+	}
+
+}
